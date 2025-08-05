f880dbb8cd44f3611f06fc63bd0f6432500e224b
Chinonye Ekpendu <alexekpendu001@gmail.com>
2025-08-05 14:54:28 -0700

    DOM elements

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // DOM elements
+    const cardholderInput = document.querySelector('#cardholder');
+    const cardNumberInput = document.querySelector('#cardnumber');
+    const monthInput = document.querySelector('.monthInput input');
+    const yearInput = document.querySelector('.yearInput input');
+    const cvcInput = document.querySelector('.cvc input');
+    const cardNumberDisplay = document.querySelector('.cardNumber');
+    const cardNameDisplay = document.querySelector('.cardName');
+    const cardExpDateDisplay = document.querySelector('.cardExpDate');
+    const cardCVCDisplay = document.querySelector('.cardCVC');
+    const form = document.querySelector('.form-container');
+    const button = form.querySelector('button');
+});
